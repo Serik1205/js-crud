2541ebbeb119a0a9c1035778e3c8571ec2820c32
Serik1205 <barba_sl@i.ua>
2023-09-13 00:06:25 +0300

    upd3 3-purchase

--- src/route/index.js
+++ src/route/index.js
@@ -4,7 +4,84 @@ const express = require('express')
 const router = express.Router()
 
 // ================================================================
-class Product { }
+class Product {
+	static #list = []
+	static #count = 0
+	constructor(img, title, description, category, price) {
+		this.id = ++Product.#count
+		this.img = img
+		this.title = title
+		this.description = description
+		this.category = category
+		this.price = price
+	}
+	static add = (
+		img,
+		title,
+		description,
+		category,
+		price,
+	) => {
+		const newProduct = new Product(
+			img,
+			title,
+			description,
+			category,
+			price,
+		)
+		this.#list.push(newProduct)
+	}
+	static getList = () => {
+		return this.#list
+	}
+	static getById = (id) => {
+		return this.#list.find((product) => product.id === id)
+	}
+	static getRandomList = (id) => {
+		const filteredList = this.#list.filter(
+			(product) => product.id != id,
+		)
+		const shuffledList = filteredList.sort(
+			() => Math.random() - 0.5
+		)
+		return shuffledList.slice(0.3);
+	}
+}
+
+Product.add(
+	'/img/comp.png',
+	`Комп'ютер Artline Gaming (X43v31) AMD Ryzen 5 3600/`,
+	`AMD Ryzen 5 3600 (3.6 - 4.2 ГГц) / RAM 16 ГБ / HDD 1 ТБ + SSD 480 ГБ / nVidia GeForce RTX 3050, 8 ГБ / без ОД / LAN / без ОС`,
+	[
+		{ id: 1, text: 'Готовий до відправки' },
+		{ id: 2, text: 'Топ продажів' },
+	],
+	27000,
+
+)
+Product.add(
+	'/img/comp2.png',
+	`Комп'ютер COBRA Advanced (I11F.8.H1S2.15T.13356) Intel`,
+	`Intel Core i3-10100F (3.6 - 4.3 ГГц) / RAM 8 ГБ / HDD 1 ТБ + SSD 240 ГБ / GeForce GTX 1050 Ti, 4 ГБ / без ОД / LAN / Linux`,
+	[
+		{ id: 1, text: 'Готовий до відправки' },
+		{ id: 2, text: 'Топ продажів' },
+	],
+	17000,
+
+)
+Product.add(
+	'/img/comp3.png',
+	`Комп'ютер ARTLINE Gaming by ASUS TUF v119 (TUFv119`,
+	`Intel Core i9-13900KF (3.0 - 5.8 ГГц) / RAM 64 ГБ / SSD 2 ТБ (2 x 1 ТБ) / nVidia GeForce RTX 4070 Ti, 12 ГБ / без ОД / LAN / Wi-Fi / Bluetooth / без ОС`,
+	[
+		{ id: 1, text: 'Готовий до відправки' },
+		{ id: 2, text: 'Топ продажів' },
+	],
+	113109,
+
+)
+
 
 
 
@@ -34,28 +111,59 @@ router.get('/purchase-index', function (req, res) {
 	res.render('purchase-index', {
 		// вказуємо назву папки контейнера, в якій знаходяться наші стилі
 		style: 'purchase-index',
+		title: 'Товари',
+		description: `Комп'ютери та ноутбуки/Комп'ютери, неттопи, моноблоки`,
+
 		data: {
-			img: '/img/comp.png',
-			title: "Комп'ютер Artline Gaming (X43v31) AMD Ryzen 5 3600/",
-			description: "AMD Ryzen 5 3600 (3.6 - 4.2 ГГц) / RAM 16 ГБ / HDD 1 ТБ + SSD 480 ГБ / nVidia GeForce RTX 3050, 8 ГБ / без ОД / LAN / без ОС",
-			category: [
-				{ id: 1, text: "Готовий до відправки" },
-				{ id: 2, text: "ТОП Продіжів" },
-			],
-			price: 27000,
-			img: '/img/comp2.png',
-			title: "Комп'ютер COBRA Advanced (I11F.8.H1S2.15T.13356) Intel",
-			description: "Intel Core i3-10100F (3.6 - 4.3 ГГц) / RAM 8 ГБ / HDD 1 ТБ + SSD 240 ГБ / GeForce GTX 1050 Ti, 4 ГБ / без ОД / LAN / Linux",
-			category: [
-				{ id: 1, text: "Готовий до відправки" },
-				{ id: 2, text: "ТОП Продіжів" },
-			],
-			price: 17000,
+			title: 'Товари',
+			subtitle:
+				"Комп'ютери та ноутбуки/Комп'ютери, неттопи, моноблоки",
+			list: Product.getList(),
 		},
 	})
 })
 // ↑↑ сюди вводимо JSON дані
+router.get('/purchase-product', function (req, res) {
+	const id = Number(req.query.id)
+	res.render('purchase-product', {
+		// вказуємо назву папки контейнера, в якій знаходяться наші стилі
+		style: 'purchase-product',
 
+		data: {
+			list: Product.getRandomList(id),
+			product: Product.getById(id),
+		},
+	})
+})
+
+router.post('/purchase-create', function (req, res) {
+	const id = Number(req.query.id)
+	const amount = Number(req.body.amount)
+	console.log(id, amount);
+	if (amount < 1) {
+		return res.render('alert', {
+			// вказуємо назву папки контейнера, в якій знаходяться наші стилі
+			style: 'alert',
+
+			data: {
+				message: "Помилка",
+				info: "Некоректна кількість товару",
+				link: `purchase-product?id=${id}`
+			},
+		})
+	}
+
+
+	res.render('purchase-product', {
+		// вказуємо назву папки контейнера, в якій знаходяться наші стилі
+		style: 'purchase-product',
+
+		data: {
+			list: Product.getRandomList(id),
+			product: Product.getById(id),
+		},
+	})
+})
 
 
 // Підключаємо роутер до бек-енду
